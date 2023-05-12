e27f833495478b4e3937b096f6472bf97f3d1add
emptystill <mateodpl99@hotmail.com>
2023-05-12 15:56:22 -0500

    Libraries

---
+++ vars/buildPipeline.groovy
@@ -0,0 +1,25 @@
+def call() {
+  pipeline {
+    agent any
+
+    tools {
+      // Especifica la versión de Maven
+      maven 'Maven_3.9.0'
+    }
+
+    stages {
+      stage('Checkout') {
+        steps {
+          checkout scm
+        }
+      }
+
+      stage('Build') {
+        steps {
+          // Ejecuta el comando Maven con la versión especificada
+          bat 'mvn clean package'
+        }
+      }
+    }
+  }
+}
---
+++ vars/codeAnalysis.groovy
@@ -0,0 +1,16 @@
+def call() {
+  pipeline {
+    agent any
+
+    stages {
+      stage('Static Code Analysis') {
+        steps {
+          // Ejecuta el análisis estático del código con SonarQube
+          withSonarQubeEnv('SonarQube') {
+            bat 'mvn sonar:sonar'
+          }
+        }
+      }
+    }
+  }
+}
