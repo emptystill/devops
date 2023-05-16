6698f7560218bb05c8ce02119b81e832e28e024d
emptystill <mateodpl99@hotmail.com>
2023-05-16 12:38:54 -0500

    s

--- vars/buildPipeline.groovy
+++ vars/buildPipeline.groovy
@@ -1,5 +1,5 @@
 def call(script, args) {
       echo "+++++++${script}+++++++"
       echo "+++++++${script.tool 'Maven_3.9.0'}+++++++"
-      script.bat "${script.tool 'Maven_3.9.0'}/bin/mvn.bat -o ${args}"
+      script.bat "${script.tool 'Maven_3.9.0'}/bin/mvn.cmd -o ${args}"
 }
