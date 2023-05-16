f4eb0f366aa887e6f54c9b28a031c78027c6de0b
emptystill <mateodpl99@hotmail.com>
2023-05-16 10:18:55 -0500

    c

--- vars/Pipeline.groovy
+++ vars/Pipeline.groovy
@@ -1,21 +1,13 @@
 def call() {
-  agent any
-
-  tools {
-    // Especifica la versión de Maven
-    maven 'Maven_3.9.0'
+  stage('Checkout') {
+    steps {
+      checkout scm
+    }
   }
 
-  stages {
-    stage('Checkout') {
-      steps {
-        checkout scm
-      }
-    }
-
-    stage('Build') {
-      steps {
-        // Ejecuta el comando Maven con la versión especificada
+  stage('Build') {
+    steps {
+      withMaven(maven: 'Maven_3.9.0') {
         bat 'mvn clean package'
       }
     }
