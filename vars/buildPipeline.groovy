615bb62af8330657282102a96f81e86bb74f92ca
emptystill <mateodpl99@hotmail.com>
2023-05-16 12:33:30 -0500

    s

--- vars/buildPipeline.groovy
+++ vars/buildPipeline.groovy
@@ -1,3 +1,3 @@
-def call(script, asrgs) {
-      script.sh "${script.tool 'Maven_3.9.0'}/bin/mvn -s ${script.env.HOME}/jenkins.xml -o ${args}"
+def call(script, args) {
+      script.sh "${script.tool 'Maven_3.9.0'}/bin/mvn -o ${args}"
 }
