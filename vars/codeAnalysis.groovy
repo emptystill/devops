435c0d4dec36c3edab6d9fb69615937a4aa85e35
emptystill <mateodpl99@hotmail.com>
2023-05-16 16:11:14 -0500

    c

---
+++ vars/codeAnalysis.groovy
@@ -0,0 +1,3 @@
+def call(script) {
+      script.bat "mvn clean verify sonar:sonar -Dsonar.projectKey=library-analysis -Dsonar.host.url=http://localhost:9000 -Dsonar.login=sqp_bdf628850b7cc4298af968a089d3ca1f46b594b7"
+}
