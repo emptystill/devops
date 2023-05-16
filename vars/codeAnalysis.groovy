9847188c8b829b8e4b3eb36b9d064fa7ffc16f70
emptystill <mateodpl99@hotmail.com>
2023-05-16 15:41:01 -0500

    c

--- vars/codeAnalysis.groovy
+++ vars/codeAnalysis.groovy
@@ -1,5 +1,5 @@
 def call(script, args) {
       echo "+++++++${script}+++++++"
       echo "+++++++${script.tool 'SonarQube'}+++++++"
-      script.bat "${script.tool 'SonarQube'}/bin/mvn.cmd -o ${args}"
+      script.bat "${script.tool 'SonarQube'}/bin/sonar-scanner.bat -o ${args}"
 }
