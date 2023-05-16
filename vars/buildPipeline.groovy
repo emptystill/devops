318c2701f5ccf0f93e1ff87b5b4ad7d02ea264c2
emptystill <mateodpl99@hotmail.com>
2023-05-16 12:32:22 -0500

    s

--- vars/buildPipeline.groovy
+++ vars/buildPipeline.groovy
@@ -1,5 +1,3 @@
-class Utilities {
-  static def mvn(script, args) {
-    script.sh "${script.tool 'Maven'}/bin/mvn -s ${script.env.HOME}/jenkins.xml -o ${args}"
-  }
+def call(script, asrgs) {
+      script.sh "${script.tool 'Maven_3.9.0'}/bin/mvn -s ${script.env.HOME}/jenkins.xml -o ${args}"
 }
