bd717d5770d459dd92b781e9fffa97aea700abc9
emptystill <mateodpl99@hotmail.com>
2023-05-18 17:47:49 -0500

    c

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,4 +1,4 @@
 def call(script) {
   // Comando para construir la imagen de Docker con los parámetros especificados
-  script.bat "docker build -t javaaplication"
+  script.bat "docker build -t javaaplication ."
 }
