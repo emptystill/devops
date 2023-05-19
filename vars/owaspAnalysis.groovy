bdd8b1549cd9f22b447d6c30d9f05a5955040e82
emptystill <mateodpl99@hotmail.com>
2023-05-19 09:54:34 -0500

    cambio

--- vars/owaspAnalysis.groovy
+++ vars/owaspAnalysis.groovy
@@ -1,5 +1,5 @@
 def call(script) {
   // Comando para enviar la imagen de Docker con los parámetros especificados a Docker Hub
   script.bat "docker pull owasp/zap2docker-stable"
-  script.bat "docker run -v datosowasp1:/zap/wrk/:rw --user root -t owasp/zap2docker-stable zap-full-scan.py -t javacontainer -r datosowasp1.html -1"  
+  script.bat "docker run -v datosowasp1:/zap/wrk/:rw --user root -t owasp/zap2docker-stable zap-full-scan.py -t http://javacontainer:8081 -r datosowasp1.html -1"  
 }
