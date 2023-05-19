989887e2ea073173ddb7ecd11d64f275b50b1844
emptystill <mateodpl99@hotmail.com>
2023-05-18 19:02:11 -0500

    cambios

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -1,6 +1,6 @@
 def call(script) {
   // Comando para enviar la imagen de Docker con los parámetros especificados a Docker Hub
-  script.bat "docker login -u mateopulido99 --password-dckr_pat_96eVTLvulJZ_DLK2EqlbVEJrXV0"
+  script.bat "docker login -u mateopulido -p dckr_pat_96eVTLvulJZ_DLK2EqlbVEJrXV0"
   script.bat "docker tag javaaplication mateopulido99/javaaplication:v1.0"
   script.bat "docker push mateopulido99/javaaplication:v1.0"
 }
