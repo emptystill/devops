0752e93cf8c95f4d0854faf5ec7b03ffd71630a1
emptystill <mateodpl99@hotmail.com>
2023-05-18 20:21:01 -0500

    cambios

--- vars/dockerDeploy.groovy
+++ vars/dockerDeploy.groovy
@@ -1,4 +1,4 @@
 def call(script) {
   // Comando para iniciar un nuevo contenedor con la imagen especificada
-  script.bat "docker run -p 8081:8080 mateopulido99/javaaplication:v1.0"
+  script.bat "docker run -d --name javacontainer -p 8081:8080 mateopulido99/javaaplication:v1.0"
 }
