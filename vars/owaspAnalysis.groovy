5faeb8167da9127cc79071b8242f40f7dbde8f1d
emptystill <mateodpl99@hotmail.com>
2023-05-22 11:41:36 -0500

    def

--- vars/owaspAnalysis.groovy
+++ vars/owaspAnalysis.groovy
@@ -2,7 +2,7 @@ def call(script) {
   // Comando para enviar la imagen de Docker con los parámetros especificados a Docker Hub
   script.bat "docker pull owasp/zap2docker-stable"
   // script.bat "docker run -v datosowasp1:/zap/wrk/:rw --network java_network --name owasp-container --user root -t owasp/zap2docker-stable zap-full-scan.py -t http://javacontainer:8080 -r datosowasp1.html 2> /dev/null" 
-  def exitCode = bat(script: 'script.bat "docker run -v datosowasp1:/zap/wrk/:rw --network java_network --name owasp-container --user root -t owasp/zap2docker-stable zap-full-scan.py -t http://javacontainer:8080 -r datosowasp1.html"', returnStatus: true)
+  def exitCode = bat(script: 'docker run -v datosowasp1:/zap/wrk/:rw --network java_network --name owasp-container --user root -t owasp/zap2docker-stable zap-full-scan.py -t http://javacontainer:8080 -r datosowasp1.html', returnStatus: true)
   if (exitCode == 2) {
     echo 'El comando retornó el código de salida 2, pero lo estamos ignorando.'
     // Realiza acciones adicionales si es necesario
