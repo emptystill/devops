00c73cc38e7839c66c957e89bfb655db6fe96154
emptystill <mateodpl99@hotmail.com>
2023-05-26 12:45:37 -0500

    c

--- vars/javaPipe.groovy
+++ vars/javaPipe.groovy
@@ -5,46 +5,15 @@ def call(String scmUrl) {
   pipeline {
     agent any
 
-    stages {
-      stage('Capture Branch') {
-        steps {
-          script {
-            def branchName = ""
-
-            if (env.BRANCH_NAME == ~/feature\/.*/) {
-              branchName = env.BRANCH_NAME
-            } else {
-              error("Unsupported branch: ${env.BRANCH_NAME}")
-            }
-
-            echo "Captured branch name: ${branchName}"
-          }
-        }
+    stage('Build') {
+      steps {
+        buildPipeline(this, 'clean package')
       }
+    }
 
-      stage('Clone Repository') {
-        steps {
-          script {
-            def repositoryUrl = scmUrl
-            def repositoryName = repositoryUrl.substring(repositoryUrl.lastIndexOf("/") + 1, repositoryUrl.lastIndexOf(".git"))
-
-            dir(repositoryName) {
-              checkout scm
-            }
-          }
-        }
-      }
-
-      stage('Build') {
-        steps {
-          buildPipeline(this, 'clean package')
-        }
-      }
-
-      stage('CodeAnalysis') {
-        steps {
-          codeAnalysis(this)
-        }
+    stage('CodeAnalysis') {
+      steps {
+        codeAnalysis(this)
       }
     }
   }
