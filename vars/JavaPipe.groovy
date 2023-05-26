6cf317f9c5c5848919a0c58eb90320a5e8317350
emptystill <mateodpl99@hotmail.com>
2023-05-26 11:34:51 -0500

    c

---
+++ vars/JavaPipe.groovy
@@ -0,0 +1,48 @@
+@Library(['build-library', 'sonarqube-library']) _
+
+pipeline {
+  agent any
+  
+  stages {
+    stage('Capture Branch') {
+      steps {
+        script {
+          def branchName = ""
+          if (env.BRANCH_NAME.startsWith("refs/pull/")) {
+            branchName = sh(returnStdout: true, script: 'echo ${env.CHANGE_ID}').trim()
+          } else if (env.BRANCH_NAME.startsWith("feature/")) {
+            branchName = env.BRANCH_NAME
+          } else {
+            error("Unsupported branch: ${env.BRANCH_NAME}")
+          }
+          echo "Captured branch name: ${branchName}"
+        }
+      }
+    }
+    
+    stage('Clone Repository') {
+      steps {
+        script {
+          def repositoryUrl = "https://github.com/emptystill/JavaAplication.git"
+          def repositoryName = repositoryUrl.substring(repositoryUrl.lastIndexOf("/") + 1, repositoryUrl.lastIndexOf(".git"))
+          
+          dir(repositoryName) {
+            checkout scm
+          }
+        }
+      }
+    }
+    
+    stage('Build') {
+      steps {
+        buildPipeline(this, 'clean package')
+      }
+    }
+    
+    stage('CodeAnalysis') {
+      steps {
+        codeAnalysis(this)
+      }
+    }
+  }
+}
