59f822d3036e890a3511fa0abb606942bf3f5652
emptystill <mateodpl99@hotmail.com>
2023-05-26 11:49:51 -0500

    c

--- vars/JavaPipe.groovy
+++ vars/JavaPipe.groovy
@@ -1,47 +1,50 @@
 @Library(['build-library', 'sonarqube-library']) _
 
-pipeline {
-  agent any
-  
-  stages {
-    stage('Capture Branch') {
-      steps {
-        script {
-          def branchName = ""
-          if (env.BRANCH_NAME.startsWith("refs/pull/")) {
-            branchName = sh(returnStdout: true, script: 'echo ${env.CHANGE_ID}').trim()
-          } else if (env.BRANCH_NAME.startsWith("feature/")) {
-            branchName = env.BRANCH_NAME
-          } else {
-            error("Unsupported branch: ${env.BRANCH_NAME}")
-          }
-          echo "Captured branch name: ${branchName}"
-        }
-      }
-    }
-    
-    stage('Clone Repository') {
-      steps {
-        script {
-          def repositoryUrl = "https://github.com/emptystill/JavaAplication.git"
-          def repositoryName = repositoryUrl.substring(repositoryUrl.lastIndexOf("/") + 1, repositoryUrl.lastIndexOf(".git"))
-          
-          dir(repositoryName) {
-            checkout scm
+def call(String scmUrl) {
+
+  pipeline {
+    agent any
+
+    stages {
+      stage('Capture Branch') {
+        steps {
+          script {
+            def branchName = ""
+            if (env.BRANCH_NAME.startsWith("refs/pull/")) {
+              branchName = sh(returnStdout: true, script: 'echo ${env.CHANGE_ID}').trim()
+            } else if (env.BRANCH_NAME.startsWith("feature/")) {
+              branchName = env.BRANCH_NAME
+            } else {
+              error("Unsupported branch: ${env.BRANCH_NAME}")
+            }
+            echo "Captured branch name: ${branchName}"
           }
         }
       }
-    }
-    
-    stage('Build') {
-      steps {
-        buildPipeline(this, 'clean package')
+
+      stage('Clone Repository') {
+        steps {
+          script {
+            def repositoryUrl = scmUrl
+            def repositoryName = repositoryUrl.substring(repositoryUrl.lastIndexOf("/") + 1, repositoryUrl.lastIndexOf(".git"))
+
+            dir(repositoryName) {
+              checkout scm
+            }
+          }
+        }
       }
-    }
-    
-    stage('CodeAnalysis') {
-      steps {
-        codeAnalysis(this)
+
+      stage('Build') {
+        steps {
+          buildPipeline(this, 'clean package')
+        }
+      }
+
+      stage('CodeAnalysis') {
+        steps {
+          codeAnalysis(this)
+        }
       }
     }
   }
