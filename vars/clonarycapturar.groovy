962f8bbae114443de64a70e7a83b16011dd569ed
emptystill <mateodpl99@hotmail.com>
2023-05-26 15:52:27 -0500

    c

--- vars/clonarycapturar.groovy
+++ vars/clonarycapturar.groovy
@@ -1,13 +1,14 @@
 def call(script) {
-  script.bat "GitSCM git = new GitSCM()
-  git.userRemoteConfigs([
-    [url: scmUrl]
-  ])
-  git.branches([
-    [name: 'feature']
-  ])
-  git.extensions([
-    [$class: 'CleanBeforeCheckout']
-  ])
-  "
+  script.bat """
+    GitSCM git = new GitSCM()
+    git.userRemoteConfigs([
+        [url: scmUrl]
+    ])
+    git.branches([
+        [name: 'feature']
+    ])
+    git.extensions([
+        [$class: 'CleanBeforeCheckout']
+    ])
+  """
 }
