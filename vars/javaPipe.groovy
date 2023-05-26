08529d9e358a6ed87ea35a16a95f2af3948710eb
emptystill <mateodpl99@hotmail.com>
2023-05-26 12:47:03 -0500

    c

--- vars/javaPipe.groovy
+++ vars/javaPipe.groovy
@@ -5,6 +5,19 @@ def call(String scmUrl) {
   pipeline {
     agent any
 
+    stage('Clone Repository') {
+      steps {
+        script {
+          def repositoryUrl = scmUrl
+          def repositoryName = repositoryUrl.substring(repositoryUrl.lastIndexOf("/") + 1, repositoryUrl.lastIndexOf(".git"))
+
+          dir(repositoryName) {
+            checkout scm
+          }
+        }
+      }
+    }
+
     stage('Build') {
       steps {
         buildPipeline(this, 'clean package')
