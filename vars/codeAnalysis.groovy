870fe20747bc42fd10bfc582a0fc5a3dae5ff1d8
emptystill <mateodpl99@hotmail.com>
2023-05-29 17:49:37 -0500

    s

--- vars/codeAnalysis.groovy
+++ vars/codeAnalysis.groovy
@@ -1,17 +1,13 @@
-import org.sonarsource.scanner.jenkins.pipeline.*
-
 def call(script) {
-    script.withSonarQubeEnv('SonarQube') {
-        def scanner = new SonarQubeScanner()
-        
-        scanner.waitForQualityGate() // Esperar al Quality Gate
-
-        // Realizar otras operaciones en tu librería
-        // ...
+  script.withSonarQubeEnv('SonarQube') {
+    bat "mvn clean verify sonar:sonar -Dsonar.projectKey=library-analysis -Dsonar.host.url=http://localhost:9000 -Dsonar.login=sqp_bdf628850b7cc4298af968a089d3ca1f46b594b7"
+    timeout(time: 1, unit: 'HOURS') {
+      waitForQualityGate abortPipeline: true
     }
-    script.bat "mvn clean verify sonar:sonar -Dsonar.showProfiling=true -Dsonar.projectKey=library-analysis -Dsonar.host.url=http://localhost:9000 -Dsonar.login=sqp_bdf628850b7cc4298af968a089d3ca1f46b594b7"
+  }
+  // script.bat "mvn clean verify sonar:sonar -Dsonar.projectKey=library-analysis -Dsonar.host.url=http://localhost:9000 -Dsonar.login=sqp_bdf628850b7cc4298af968a089d3ca1f46b594b7"
 
-    // Verificación del quality gate utilizando el comando curl y findstr
-    script.bat 'curl -s -X GET -u sqp_bdf628850b7cc4298af968a089d3ca1f46b594b7: "http://localhost:9000/api/qualitygates/project_status?projectKey=library-analysis"'
- 
+  // Verificación del quality gate utilizando el comando curl y findstr
+  script.bat 'curl -s -X GET -u sqp_bdf628850b7cc4298af968a089d3ca1f46b594b7: "http://localhost:9000/api/qualitygates/project_status?projectKey=library-analysis"'
+
 }
