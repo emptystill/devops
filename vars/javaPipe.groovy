abd4f504c68ac82b640cbd8629bb974a5318e85b
emptystill <mateodpl99@hotmail.com>
2023-05-29 13:19:53 -0500

    c

--- vars/javaPipe.groovy
+++ vars/javaPipe.groovy
@@ -7,7 +7,7 @@ def call(String scmUrl) {
       stage('Clonar y Capturar') {
         steps {
           script {
-            clonarycapturar()
+            clonarycapturar('feature')
           }          
         }
       }
