cf3a1f6b27f091bf39438816397e72fb4f0ce20c
emptystill <mateodpl99@hotmail.com>
2023-05-29 16:29:18 -0500

    c

--- vars/codeAnalysis.groovy
+++ vars/codeAnalysis.groovy
@@ -1,8 +1,12 @@
 def call(script) {
     script.bat "mvn clean verify sonar:sonar -Dsonar.projectKey=library-analysis -Dsonar.host.url=http://localhost:9000 -Dsonar.login=sqp_bdf628850b7cc4298af968a089d3ca1f46b594b7"
 
-    // Verificación del quality gate utilizando el comando curl
+    // Verificación del quality gate utilizando el comando curl y findstr
     script.bat '''
-        curl -s -X GET -u sqp_bdf628850b7cc4298af968a089d3ca1f46b594b7: "http://localhost:9000/api/qualitygates/project_status?projectKey=library-analysis" | grep -q '"status":"ERROR"' && (echo Quality gate failed. Pipeline halted. && exit 1)
+        curl -s -X GET -u sqp_bdf628850b7cc4298af968a089d3ca1f46b594b7: "http://localhost:9000/api/qualitygates/project_status?projectKey=library-analysis" | findstr /C:"""status"":""ERROR""" > nul
+        if %errorlevel% equ 0 (
+            echo Quality gate failed. Pipeline halted.
+            exit 1
+        )
     '''
 }
