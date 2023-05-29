affc0fac5abf1c25827ef334b492141cf556872f
emptystill <mateodpl99@hotmail.com>
2023-05-29 11:15:09 -0500

    cambios

--- vars/buildPipeline.groovy
+++ vars/buildPipeline.groovy
@@ -1,5 +1,5 @@
 def call(script, args) {
-      echo "+++++++${script}+++++++"
-      echo "+++++++${script.tool 'Maven_3.9.0'}+++++++" //Para conocer la ruta de las herramientas
-      script.bat "mvn clean package"
+  script.withMaven(maven: 'Maven_3.9.0') {
+    script.bat "mvn package"
+  }
 }
