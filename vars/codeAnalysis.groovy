4808a53f337b4a0491bfbe8fdf83418993ce35b1
emptystill <mateodpl99@hotmail.com>
2023-05-29 15:52:43 -0500

    cambios

--- vars/codeAnalysis.groovy
+++ vars/codeAnalysis.groovy
@@ -3,6 +3,6 @@ def call(script) {
 
     // Verificación del quality gate utilizando la API de SonarQube
     script.bat """
-        curl -X GET -u sqp_bdf628850b7cc4298af968a089d3ca1f46b594b7: 'http://localhost:9000/api/qualitygates/project_status?projectKey=library-analysis'
+        curl -X GET -u sqp_bdf628850b7cc4298af968a089d3ca1f46b594b7: "http://localhost:9000/api/qualitygates/project_status?projectKey=library-analysis"
     """
 }
