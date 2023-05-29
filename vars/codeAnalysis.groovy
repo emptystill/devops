d687411f1748cb3adbd8a68b75f5c1f1c36b5826
emptystill <mateodpl99@hotmail.com>
2023-05-29 18:31:24 -0500

    s

--- vars/codeAnalysis.groovy
+++ vars/codeAnalysis.groovy
@@ -1,6 +1,7 @@
 def call(script) {
   script.withSonarQubeEnv('SonarQube') {
-    bat "mvn clean verify sonar:sonar -Dsonar.projectKey=library-analysis -Dsonar.host.url=http://localhost:9000 -Dsonar.login=sqp_bdf628850b7cc4298af968a089d3ca1f46b594b7"
+    // bat "mvn clean verify sonar:sonar -Dsonar.projectKey=library-analysis -Dsonar.host.url=http://localhost:9000 -Dsonar.login=sqp_bdf628850b7cc4298af968a089d3ca1f46b594b7"
+    bat "mvn clean verify sonar:sonar"
   }
   script.withSonarQubeEnv('SonarQube') {
     timeout(time: 3, unit: 'MINUTES') {
