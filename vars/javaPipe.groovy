56ae13acd0fc45e9235d757be5fb914ca5d3e516
emptystill <mateodpl99@hotmail.com>
2023-05-29 11:23:42 -0500

    c

---
+++ vars/javaPipe.groovy
@@ -0,0 +1,34 @@
+// @Library(['build-library', 'sonarqube-library']) _
+
+def call(String scmUrl) {
+
+  pipeline {
+    agent any
+
+    stages {
+      stage('Clonar y Capturar') {
+        steps {
+          script {
+            clonarycapturar('feature')
+          }          
+        }
+      }
+
+      stage('Build') {
+        steps {
+          script {
+            buildPipeline(this, 'clean package')
+          }
+        }
+      }
+
+      stage('CodeAnalysis') {
+        steps {
+          script {
+            codeAnalysis(this)
+          }
+        }
+      }
+    }
+  }
+}
