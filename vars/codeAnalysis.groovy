636441ffb24627023c9f4981aa53596d4f2c3ca9
emptystill <mateodpl99@hotmail.com>
2023-05-29 19:25:40 -0500

    s

--- vars/codeAnalysis.groovy
+++ vars/codeAnalysis.groovy
@@ -5,7 +5,9 @@ def call(script) {
   }
   script.withSonarQubeEnv('SonarQube') {
     timeout(time: 3, unit: 'MINUTES') {
+        print "Entrando en la espera"
       waitForQualityGate abortPipeline: true
+        print "Saliendo de la espera"
     }
   }
   // script.bat "mvn clean verify sonar:sonar -Dsonar.projectKey=library-analysis -Dsonar.host.url=http://localhost:9000 -Dsonar.login=sqp_bdf628850b7cc4298af968a089d3ca1f46b594b7"
