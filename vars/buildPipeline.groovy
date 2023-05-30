551882cc87dc53ae2c7ac995f60d70e5c61224fb
emptystill <mateodpl99@hotmail.com>
2023-05-30 09:54:57 -0500

    se eliminar para hacer merge

--- vars/buildPipeline.groovy
+++
@@ -1,5 +0,0 @@
-def call(script, args) {
-      echo "+++++++${script}+++++++"
-      echo "+++++++${script.tool 'Maven_3.9.0'}+++++++" //Para conocer la ruta de las herramientas
-      script.bat "mvn clean package"
-}
