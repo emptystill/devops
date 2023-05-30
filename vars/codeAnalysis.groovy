67313c692ad467b7d3b25ba13c90657fccbd249a
emptystill <mateodpl99@hotmail.com>
2023-05-29 19:57:16 -0500

    s

--- vars/codeAnalysis.groovy
+++ vars/codeAnalysis.groovy
@@ -1,18 +1,10 @@
 def call(script) {
   script.withSonarQubeEnv('SonarQube') {
-    // bat "mvn clean verify sonar:sonar -Dsonar.projectKey=library-analysis -Dsonar.host.url=http://localhost:9000 -Dsonar.login=sqp_bdf628850b7cc4298af968a089d3ca1f46b594b7"
     bat "mvn clean verify sonar:sonar"
   }
   script.withSonarQubeEnv('SonarQube') {
     timeout(time: 3, unit: 'MINUTES') {
-        print "Entrando en la espera"
       waitForQualityGate abortPipeline: true
-        print "Saliendo de la espera"
     }
   }
-  // script.bat "mvn clean verify sonar:sonar -Dsonar.projectKey=library-analysis -Dsonar.host.url=http://localhost:9000 -Dsonar.login=sqp_bdf628850b7cc4298af968a089d3ca1f46b594b7"
-
-  // Verificación del quality gate utilizando el comando curl y findstr
-  //   script.bat 'curl -s -X GET -u sqp_bdf628850b7cc4298af968a089d3ca1f46b594b7: "http://localhost:9000/api/qualitygates/project_status?projectKey=library-analysis"'
-
 }
