6dc948a320ae2e6c29fcf4b407852225fd15144f
emptystill <mateodpl99@hotmail.com>
2023-05-30 13:32:59 -0500

    cambio

--- vars/javaPipe.groovy
+++ vars/javaPipe.groovy
@@ -9,6 +9,7 @@ def call(String scmUrl) {
           }
         }
       }
+      
       stage('Build Artifact') {
         steps {
           script {
@@ -16,6 +17,7 @@ def call(String scmUrl) {
           }
         }
       }
+      
       stage('SonarAnalysis y QualityGate') {
         steps {
           script {
@@ -24,46 +26,47 @@ def call(String scmUrl) {
         }
       }
 
-      ///variables de entorno///
       stage('Phase 2 Deploy') {
         when {
-          script {
-            beforeAgent true
-            expression {
-              return env.GIT_BRANCH == 'origin/develop' || env.GIT_BRANCH == 'origin/master'
-            }
+          beforeAgent true
+          expression {
+            return env.GIT_BRANCH == 'origin/develop' || env.GIT_BRANCH == 'origin/master'
           }
         }
-        stage('Docker Build') {
-          steps {
-            script {
-              dockerBuild(this)
+        steps {
+          stage('Docker Build') {
+            steps {
+              script {
+                dockerBuild(this)
+              }
             }
           }
-        }
-        stage('Docker Push') {
-          steps {
-            script {
-              dockerPush(this)
+
+          stage('Docker Push') {
+            steps {
+              script {
+                dockerPush(this)
+              }
             }
           }
-        }
-        stage('Docker Deploy') {
-          steps {
-            script {
-              dockerDeploy(this)
+
+          stage('Docker Deploy') {
+            steps {
+              script {
+                dockerDeploy(this)
+              }
             }
           }
-        }
-        stage('Owasp Analysis') {
-          steps {
-            script {
-              owaspAnalysis(this)
+
+          stage('Owasp Analysis') {
+            steps {
+              script {
+                owaspAnalysis(this)
+              }
             }
           }
         }
       }
-
     }
   }
 }
