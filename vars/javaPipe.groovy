086173cf1a436c983affa82580e186bc45ae9ed9
emptystill <mateodpl99@hotmail.com>
2023-05-31 14:57:41 -0500

    cambios

--- vars/javaPipe.groovy
+++ vars/javaPipe.groovy
@@ -1,64 +1,29 @@
 def call(String scmUrl) {
+
   pipeline {
     agent any
+
     stages {
-      stage('Clone and Checkout') {
+      stage('Clonar y Capturar') {
         steps {
           script {
             clonarycapturar('https://github.com/emptystill/JavaAplication.git', 'feature')
           }          
         }
       }
-      stage('Build Artifact') {
+
+      stage('Build') {
         steps {
           script {
             buildPipeline(this, 'clean package')
           }
         }
       }
-      stage('SonarAnalysis y QualityGate') {
+
+      stage('CodeAnalysis') {
         steps {
           script {
             codeAnalysis(this)
-          }
-        }
-      }
-
-    ///variables de entorno///
-      stage('Phase 2 Deploy') {
-        when {
-          script {
-            dockerBuild(this)
-            expression {return env.GIT_BRANCH == 'origin/develop' || env.GIT_BRANCH == 'origin/master'}
-          }
-        }
-      }  
-      
-      stage('Docker Build') {
-        steps {
-          script {
-            dockerBuild(this)
-          }
-        }
-      }
-      stage('Docker Push') {
-        steps {
-          script {
-            dockerPush(this)
-          }
-        }
-      }
-      stage('Docker Deploy') {
-        steps {
-          script {
-            dockerDeploy(this)
-          }
-        }
-      }
-      stage('Owasp Analysis') {
-        steps {
-          script {
-            owaspAnalysis(this)
           }
         }
       }
