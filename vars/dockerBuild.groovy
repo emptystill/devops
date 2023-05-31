744d5227560fc8c50315e493098b59b797122dcb
emptystill <mateodpl99@hotmail.com>
2023-05-31 08:17:45 -0500

    se eliman archivos innecesarios

--- vars/dockerBuild.groovy
+++
@@ -1,4 +0,0 @@
-def call(script) {
-  // Comando para construir la imagen de Docker con los parámetros especificados
-  script.bat "docker build -t javaaplication ."
-}
--- vars/owaspAnalysis.groovy
+++
@@ -1,8 +0,0 @@
-def call(script) {
-  // Comando para enviar la imagen de Docker con los parámetros especificados a Docker Hub
-  script.bat "docker pull owasp/zap2docker-stable"
-  def exitCode = bat(script: 'docker run -v datosowasp1:/zap/wrk/:rw --network java_network --name owasp-container --user root -t owasp/zap2docker-stable zap-full-scan.py -t http://javacontainer:8080 -r datosowasp1.html', returnStatus: true)
-  if (exitCode == 2) {
-    echo 'El comando retornó el código de salida 2, pero lo estamos ignorando.'
-  } 
-}
