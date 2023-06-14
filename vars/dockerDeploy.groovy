23beb65b0d05fafb784e9e4cdc9ae5068788501d
emptystill <mateodpl99@hotmail.com>
2023-06-14 15:16:12 -0500

    cambio

--- vars/dockerDeploy.groovy
+++ vars/dockerDeploy.groovy
@@ -9,15 +9,11 @@ def call(script) {
   } else {
     println "La red 'java_network' ya existe."
   }
-  
-  def containerCheck = script.bat(
-    returnStatus: true,
-    script: 'docker inspect javacontainer >nul 2>&1 && echo EXISTS || echo NOT_EXISTS'
-  )
 
-  if (containerCheck.trim() == 'NOT_EXISTS') {
-    script.bat 'docker run -d --network java_network --name javacontainer -p 8081:8080 mateopulido99/javaaplication:v1.0'
-  } else {
+  script.bat 'docker inspect javacontainer >nul 2>&1'
+  if (script.returnStatus == 0) {
     println "El contenedor 'javacontainer' ya existe."
+  } else {
+    script.bat 'docker run -d --network java_network --name javacontainer -p 8081:8080 mateopulido99/javaaplication:v1.0'
   }
 }
