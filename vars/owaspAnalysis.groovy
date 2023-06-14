6292e53a1bbcb459ac631bd489b00dd3b3b37b7d
emptystill <mateodpl99@hotmail.com>
2023-06-14 16:37:46 -0500

    cambio

--- vars/owaspAnalysis.groovy
+++ vars/owaspAnalysis.groovy
@@ -1,15 +1,5 @@
 def call(script) {
   script.bat "docker pull owasp/zap2docker-stable"
-  script.bat "docker run -v owaspscanner:/zap/wrk/:rw --network java_network --name owasp-container --user root -t owasp/zap2docker-stable zap-full-scan.py -t http://javacontainer:8080 -r fullscan-results.html -I"
-
-  def containerCheck = script.bat(
-    returnStatus: true,
-    script: 'docker inspect owasp-container >nul 2>&1'
-  )
-
-  if (containerCheck != 0) {
-    script.bat 'docker run -v datosowasp1:/zap/wrk/:rw --network java_network --name owasp-container --user root -t owasp/zap2docker-stable zap-baseline.py -t http://javacontainer:8080 -r baseline-results.html -I'
-  } else {
-    println "El contenedor 'owasp-container' ya existe, continúa con el pipeline."
-  }
+  script.bat "docker run --rm -v owaspscanner:/zap/wrk/:rw --network java_network --user root -t owasp/zap2docker-stable zap-full-scan.py -t http://javacontainer:8080 -r fullscan-results.html -I"
+  script.bat "docker run --rm -v owaspscanner:/zap/wrk/:rw --network java_network --user root -t owasp/zap2docker-stable zap-baseline.py -t http://javacontainer:8080 -r baseline-results.html -I"
 }
