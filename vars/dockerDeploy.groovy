3086e2ecf22b88e2858974bbf8749b58955beacf
emptystill <mateodpl99@hotmail.com>
2023-06-14 15:55:32 -0500

    cambio

--- vars/dockerDeploy.groovy
+++ vars/dockerDeploy.groovy
@@ -3,10 +3,21 @@ def call(script) {
     returnStatus: true,
     script: 'docker network inspect java_network >nul 2>&1'
   )
+
   if (networkCheck != 0) {
     script.bat 'docker network create java_network'
-  } else {  
+  } else {
     println "La red 'java_network' ya existe."
   }
-  script.bat 'docker run -d --network java_network --name javacontainer -p 8081:8080 mateopulido99/javaaplication:v1.0'
+
+  def containerCheck = script.bat(
+    returnStatus: true,
+    script: 'docker inspect javacontainer >nul 2>&1'
+  )
+
+  if (containerCheck != 0) {
+    script.bat 'docker run -d --network java_network --name javacontainer -p 8081:8080 mateopulido99/javaaplication:v1.0'
+  } else {
+    println "El contenedor 'javacontainer' ya existe, continúa con el pipeline."
+  }
 }
