94b308e59398cb52f0dcdc2c5c9878346413f5d5
emptystill <mateodpl99@hotmail.com>
2023-06-14 16:02:39 -0500

    cambio owasp

--- vars/owaspAnalysis.groovy
+++ vars/owaspAnalysis.groovy
@@ -1,9 +1,14 @@
 def call(script) {
-  // Comando para enviar la imagen de Docker con los parámetros especificados a Docker Hub
-  script.bat "docker pull owasp/zap2docker-stable"
-  script.bat "docker run -v datosowasp1:/zap/wrk/:rw --network java_network --name owasp-container --user root -t owasp/zap2docker-stable zap-full-scan.py -t http://javacontainer:8080 -r datosowasp1.html -I"
-  // def exitCode = bat(script: 'docker run -v datosowasp1:/zap/wrk/:rw --network java_network --name owasp-container --user root -t owasp/zap2docker-stable zap-full-scan.py -t http://javacontainer:8080 -r datosowasp1.html', returnStatus: true)
-  // if (exitCode == 2) {
-  //   echo 'El comando retornó el código de salida 2, pero lo estamos ignorando.'
-  // } 
+  script.bat 'docker pull owasp/zap2docker-stable'
+
+  def containerCheck = script.bat(
+    returnStatus: true,
+    script: 'docker inspect owasp-container >nul 2>&1'
+  )
+
+  if (containerCheck != 0) {
+    script.bat 'docker run -v datosowasp1:/zap/wrk/:rw --network java_network --name owasp-container --user root -t owasp/zap2docker-stable zap-full-scan.py -t http://javacontainer:8080 -r datosowasp1.html -I'
+  } else {
+    println "El contenedor 'owasp-container' ya existe, continúa con el pipeline."
+  }
 }
