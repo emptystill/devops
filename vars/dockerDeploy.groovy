c15b15f70caabc57c1593edcd4ec090488d7c154
emptystill <mateodpl99@hotmail.com>
2023-06-14 14:31:21 -0500

    cambio deplo

--- vars/dockerDeploy.groovy
+++ vars/dockerDeploy.groovy
@@ -1,13 +1,13 @@
 def call(script) {
   // Comando para verificar si la red ya está creada
-  def networkExists = script.returnStatus("docker network inspect java_network")
+  def networkCheck = script.bat "docker network inspect java_network", returnStatus: true
 
-  if (networkExists != 0) {
-    // La red no existe, se crea
-    script.bat "docker network create java_network"
-  } else {
+  if (networkCheck == 0) {
     // La red ya existe, se ignora la acción de crearla
     println "La red 'java_network' ya está creada."
+  } else {
+    // La red no existe, se crea
+    script.bat "docker network create java_network"
   }
 
   script.bat "docker run -d --network java_network --name javacontainer -p 8081:8080 mateopulido99/javaaplication:v1.0"
