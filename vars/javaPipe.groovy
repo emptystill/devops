a2e4a9fa13ec4fa6975670b13a0d49275dc4972d
emptystill <mateodpl99@hotmail.com>
2023-06-14 11:33:51 -0500

    cambios java

--- vars/javaPipe.groovy
+++ vars/javaPipe.groovy
@@ -1,63 +1,63 @@
 def call(String scmUrl) {
-  pipeline {
-    agent any
-    stages {
-      stage('Clone and Checkout') {
-        steps {
-          script {
-            //print "-------------------${env.GIT_BRANCH}-------------------"
-            clonarycapturar('https://github.com/emptystill/JavaAplication.git', 'feature')
-          }
-        }
-      }
+  // pipeline {
+  //   agent any
+  //   stages {
+  //     stage('Clone and Checkout') {
+  //       steps {
+  //         script {
+  //           //print "-------------------${env.GIT_BRANCH}-------------------"
+  //           clonarycapturar('https://github.com/emptystill/JavaAplication.git', 'feature')
+  //         }
+  //       }
+  //     }
       
-      stage('Build Artifact') {
-        steps {
-          script {
-            buildPipeline(this, 'clean package')
-          }
-        }
-      }
+  //     stage('Build Artifact') {
+  //       steps {
+  //         script {
+  //           buildPipeline(this, 'clean package')
+  //         }
+  //       }
+  //     }
       
-      stage('SonarAnalysis y QualityGate') {
-        steps {
-          script {
-            codeAnalysis(this)
-          }
-        }
-      }
+  //     stage('SonarAnalysis y QualityGate') {
+  //       steps {
+  //         script {
+  //           codeAnalysis(this)
+  //         }
+  //       }
+  //     }
 
-      stage('Phase 2 Deploy') {
-        when {
-          beforeAgent true
-          expression {
-            return env.GIT_BRANCH == 'origin/develop' || env.GIT_BRANCH == 'origin/master'
-          }
-        }
-        stages {
-          stage('Docker Build') {
-            steps {
-              script {
-                dockerBuild(this)
-              }
-            }
-          }
+  //     stage('Phase 2 Deploy') {
+  //       when {
+  //         beforeAgent true
+  //         expression {
+  //           return env.GIT_BRANCH == 'origin/develop' || env.GIT_BRANCH == 'origin/master'
+  //         }
+  //       }
+  //       stages {
+  //         stage('Docker Build') {
+  //           steps {
+  //             script {
+  //               dockerBuild(this)
+  //             }
+  //           }
+  //         }
 
-          stage('Docker Push') {
-            steps {
-              script {
-                dockerPush(this)
-              }
-            }
-          }
+  //         stage('Docker Push') {
+  //           steps {
+  //             script {
+  //               dockerPush(this)
+  //             }
+  //           }
+  //         }
 
-          stage('Docker Deploy') {
-            steps {
-              script {
-                dockerDeploy(this)
-              }
-            }
-          }
+  //         stage('Docker Deploy') {
+  //           steps {
+  //             script {
+  //               dockerDeploy(this)
+  //             }
+  //           }
+  //         }
 
           stage('Owasp Analysis') {
             steps {
@@ -67,7 +67,7 @@ def call(String scmUrl) {
             }
           }
         }
-      }
-    }
-  }
-}
+//       }
+//     }
+//   }
+// }
