e0746010d88e704969c78fd9f08af7d1c6943471
emptystill <mateodpl99@hotmail.com>
2023-06-14 15:06:57 -0500

    prueba check

--- vars/dockerDeploy.groovy
+++ vars/dockerDeploy.groovy
@@ -1,17 +1,23 @@
 def call(script) {
   def networkCheck = script.bat(
-    returnStdout: true,
+    returnStatus: true,
     script: 'docker network inspect java_network >nul 2>&1 && echo EXISTS || echo NOT_EXISTS'
   ).trim()
 
   if (networkCheck == 'NOT_EXISTS') {
-    // La red no existe, se crea
     script.bat 'docker network create java_network'
   } else {
-    // La red ya existe, se imprime un mensaje
     println "La red 'java_network' ya existe."
   }
+  
+  def containerCheck = script.bat(
+    returnStatus: true,
+    script: 'docker inspect javacontainer >nul 2>&1 && echo EXISTS || echo NOT_EXISTS'
+  ).trim()
 
-  // Resto de tu código aquí
-  script.bat 'docker run -d --network java_network --name javacontainer -p 8081:8080 mateopulido99/javaaplication:v1.0'
+  if (containerCheck == 'NOT_EXISTS') {
+    script.bat 'docker run -d --network java_network --name javacontainer -p 8081:8080 mateopulido99/javaaplication:v1.0'
+  } else {
+    println "El contenedor 'javacontainer' ya existe."
+  }
 }
