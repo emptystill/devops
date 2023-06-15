25ecffa9804823f30f6f4a66826908f4671f341b
emptystill <mateodpl99@hotmail.com>
2023-06-15 15:41:29 -0500

    prueba

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -1,7 +1,9 @@
 def call(script) {
   script.withCredentials([string(credentialsId: 'dockerhub-credentials', variable: 'DOCKER_PASSWORD')]) {
-    script.bat 'echo %DOCKER_PASSWORD% | docker login -u mateopulido99 --password-stdin'
+    script.bat 'echo %DOCKER_PASSWORD% > token.txt'
+    script.bat 'docker login -u mateopulido99 --password-stdin < token.txt'
     script.bat 'docker tag javaaplication mateopulido99/javaaplication:v1.0'
     script.bat 'docker push mateopulido99/javaaplication:v1.0'
   }
 }
+
